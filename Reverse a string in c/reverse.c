707ddd15140c833bdd1a0fa924d822c54e3fe3a5
KhushTrivedi445 <168230450+KhushTrivedi445@users.noreply.github.com>
2024-04-27 01:44:38 +0530

    Add files via upload

---
+++ Reverse a string in c/reverse.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main () {
+    char a[30];
+    gets(a);
+int count =0;
+for(int i=0; a[i];i++) {
+    count++;
+}
+//printf("The length of the string is %d",count);
+
+int n=count;
+for (int i=n-1;i>=0;i--){
+    printf("%c",a[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
