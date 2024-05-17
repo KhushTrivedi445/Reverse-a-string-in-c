386da5f0b117dd34158103801fe84f159d10a4ce
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-05-17 23:35:55 +0530

    reverse.c

--- Reverse a string in c/reverse.c
+++ Reverse a string in c/reverse.c
@@ -9,7 +9,7 @@ for(int i=0; a[i];i++) {
 }
 //printf("The length of the string is %d",count);
 
-int n=count;
+int n=count; //Here value of count is stored in n
 for (int i=n-1;i>=0;i--){
     printf("%c",a[i]);
 }
